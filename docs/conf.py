e4b9051d7507096885ace2b48ce1d91592aa7706
emptymalei <emptymalei@qq.com>
2016-02-15 13:58:27 -0700

    numfig testing not working

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,10 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./_extensions'))
+
 
 
 
@@ -63,6 +66,8 @@ html_theme_options = {
 extensions = [
     'sphinx.ext.mathjax',
 ]
+
+
 
 
 latex_preamble = r"""
